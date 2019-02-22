9dc73f03f1d03a110feec49e85344e9afd34178f
Marcus Carr <marcus.carr@gmail.com>
2019-02-22 14:50:48 -0700

    Hard-code ignoreUndefined
    * Riffyn-ify the repo
    * Update `versionsFrom`
    * Hard-code in the one option we need to set:
      `Mongo.setConnectionOptions({ ignoreUndefined: false });``

--- package.js
+++ package.js
@@ -1,17 +1,17 @@
 Package.describe({
-	name: 'danwild:set-connection-options',
-	version: '0.0.1',
+	name: 'riffyn:set-connection-options',
+	version: '0.0.2',
 	// Brief, one-line summary of the package.
-	summary: 'Set additional mongo connection settings (e.g. sslCert) via $MONGO_CONNECTION_OPTIONS',
+	summary: 'Set mongo connection setting ignoreUndefined to false',
 	// URL to the Git repository containing the source code for this package.
-	git: 'https://github.com/danwild/set-connection-options',
+	git: 'https://github.com/RiffynInc/set-connection-options',
 	// By default, Meteor will default to using README.md for documentation.
 	// To avoid submitting documentation, set this field to null.
 	documentation: 'README.md'
 });
 
 Package.onUse(function (api) {
-	api.versionsFrom('1.5.2.2');
+	api.versionsFrom('1.8.0.2');
 	api.use('ecmascript');
 	api.use('meteor');
 	api.use('mongo');
@@ -22,6 +22,6 @@ Package.onUse(function (api) {
 Package.onTest(function (api) {
 	api.use('ecmascript');
 	api.use('tinytest');
-	api.use('danwild:set-connection-options');
+	api.use('riffyn:set-connection-options');
 	api.mainModule('set-connection-options-tests.js');
 });
